c8b597d1011c58d6c9c5d2aa33a8ed99b3df4ea4
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-05-28 10:12:13 +0530

    Connection and disconnect events on server and client created and listened for.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 const publicPath = path.join(`${__dirname}/../public`);
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+const server = http.createServer(app);
+// For loading the socketIO library
+const io = socketIO(server);
+
+// Listening to events
+io.on('connection', (socket) => {
+  console.log('New user has connected...');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  })
+})
+
+server.listen(port, () => {
   console.log(`Starting listening on port ${port}`);
 })
